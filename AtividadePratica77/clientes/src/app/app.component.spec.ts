06809bd90d5fa00ab54ebf545cf1214f6ec8e9ce
Renata Araujo <renata.araujofreitas@gmail.com>
2021-12-07 14:03:03 -0300

    Gerando Aplicação Angular (tentativa 3)

--- AtividadePratica77/clientes/src/app/app.component.spec.ts
+++ AtividadePratica77/clientes/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gerenciamentoclientes'`, () => {
+  it(`should have as title 'clientes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gerenciamentoclientes');
+    expect(app.title).toEqual('clientes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciamentoclientes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('clientes app is running!');
   });
 });
